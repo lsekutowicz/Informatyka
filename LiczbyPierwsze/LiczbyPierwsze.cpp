79ee30f1c4ff9a987e903e8a35317687139d85fe
lsekutowicz <lukasz@sekutowicz.pl>
2022-03-02 12:00:43 +0100

    update 1.1

---
+++ LiczbyPierwsze/LiczbyPierwsze.cpp
@@ -0,0 +1,40 @@
+﻿//Algorytm wyznaczania liczb pierwszych
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, p, lp;
+    bool t;
+
+    cin >> n;
+
+    lp = 0;
+    p = 2;
+
+    while (lp < n)
+    {
+        t = true;
+        for (i = 2; i < p; i++)
+            if (p % i == 0)
+            {
+                t = false;
+                break;
+            }
+
+        if (t)
+        {
+            cout << p << " ";
+            lp++;
+        }
+        p++;
+    }
+
+    cout << endl;
+
+    return 0;
+}

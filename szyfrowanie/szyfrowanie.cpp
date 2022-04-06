ba952c21150c238a5d0e284c1f9dcf83488edcf1
lsekutowicz <lukasz@sekutowicz.pl>
2022-04-06 12:01:26 +0200

    Komentarz

---
+++ szyfrowanie/szyfrowanie.cpp
@@ -0,0 +1,47 @@
+﻿//Szyfrowanie cezara
+//30.03.2022 & 06.04.2022
+#include <iostream>
+using namespace std;
+
+int szyfruj(int klucz, char tab[]);
+
+int main()
+{
+	char tab[1001];
+	int klucz;
+
+	cout << "Podaj wyraz skladający się z małych liter: ";
+	cin >> tab;
+
+	cout << "Podaj klucz z przedziału [-26..26]: ";
+	cin >> klucz;
+
+	szyfruj(klucz, tab);
+
+	cout << "Po zaszyfrowaniu " << tab << endl;
+
+	szyfruj(-klucz, tab);
+
+	cout << "Po rozszyfrowaniu: " << tab << endl;
+
+	return 0;
+}
+
+int szyfruj(int klucz, char tab[]) {
+	int dl = strlen(tab);
+	if (klucz >= -26 && klucz <= 26) {
+		if (klucz >= 0) {
+			for (int i = 0; i < dl; i++) {
+				if (tab[i] + klucz <= 'z') {
+					tab[i] += klucz;
+				}
+				else {
+					tab[i] = tab[i] + klucz - 26;
+				}
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < dl; i++);
+	}
+}

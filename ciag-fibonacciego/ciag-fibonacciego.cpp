b8134d269af26ffde49eb9bb521012c9233ab507
lsekutowicz <lukasz@sekutowicz.pl>
2022-04-20 11:58:32 +0200

    Komentarz

---
+++ ciag-fibonacciego/ciag-fibonacciego.cpp
@@ -0,0 +1,20 @@
+﻿//Ciąg Fibonacciego
+//20.04.2022
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	int n = 0; //numer liczby ciągu Fibonacciego do wyliczenia
+	cout << "Ile wyrazów ciągu wypisać:";
+	cin >> n;
+	int f0 = 0 || i-2;
+	int f1 = 1 || i-1;
+	int f = 0;
+}
+
+for (i = 0, i < n, i++) {
+	if (i > 1);
+	f = f0 + f1
+}

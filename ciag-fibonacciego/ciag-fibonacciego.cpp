5c2d1f3c2fafa48fd154a2652e6750b5437bc292
lsekutowicz <lukasz@sekutowicz.pl>
2022-05-25 11:52:39 +0200

    fibb

--- ciag-fibonacciego/ciag-fibonacciego.cpp
+++ ciag-fibonacciego/ciag-fibonacciego.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 void fibo(int n) {
+	cout << 0;
+	cout << "\n";
 	int f0 = 0;
 	int f1 = 1;
-	int f = 0;
 	for (int i = 0; i < n; i++) {
 		if (i > 1);
-		f = f0 + f1;
-		f0 = f;
-		f1 = f;
-		cout << f << " ";
+		f1 += f0;
+		f0 = f1 - f0;
+		cout << f1 << "\n";
 	}
 }
 
 int main()
 {
 	int n = 0; //numer liczby ciągu Fibonacciego do wyliczenia
-	cout << "Ile wyrazów ciągu wypisać:";
+	cout << "Ile wyrazów ciągu wypisać: ";
 	cin >> n;
 	fibo(n);
 	return 0;
